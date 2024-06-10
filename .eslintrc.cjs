6ae155b77fc12ccfd8cfec8e78c1fd6684b25d7b
Tomas <tomas.casabona@gmail.com>
2024-06-10 14:42:01 +0200

    inicializate project with dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier", // Asegura que este es el último en la lista para desactivar reglas conflictivas
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "prettier"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "prettier/prettier": "warn", // Asegura que los problemas de Prettier se reporten como advertencias
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import daisyui from "daisyui";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+};
