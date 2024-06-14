4967bd8ebe3d5769916456db7642b7418f48cb99
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-06-14 16:55:03 +0200

    fetch data on app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { AppRoutes } from "./routes/AppRoutes";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserData } from "./redux/thunks/userDataThunks";
+import { AppDispatch } from "./redux/store";
+import { RootState } from "./redux/store";
 
 export const App: React.FC = () => {
-  return (
-    <Router>
-      <Navbar />
-      <AppRoutes />
-    </Router>
-  );
+	const dispatch = useDispatch<AppDispatch>();
+	const userData = useSelector((state: RootState) => state.userData.data);
+	dispatch(fetchUserData());
+	console.log(userData);
+	return (
+		<Router>
+			<Navbar />
+			<AppRoutes />
+		</Router>
+	);
 };
