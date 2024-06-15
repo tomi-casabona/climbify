a7e4c7832b47594cfe0f7facabf962c2109f825b
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-06-15 10:49:50 +0200

    navbar down and home header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,54 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'poppins': ['Poppins', 'sans-serif'],
-      },
-      colors: {
-        light: {
-          bg: '#fffffc', // Cambia este valor al color de fondo deseado para el modo oscuro
-        },
-        dark: {
-          bg: '#03080E', // Cambia este valor al color de fondo deseado para el modo oscuro
-        }
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
-  plugins: [daisyui]
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#feb58b",
+          secondary: "#d3dbf4",
+          "secondary-darker": "#bec6e0",
+          accent: "#bb3c43",
+          neutral: "#12161c",
+          "base-100": "#fffffc",
+
+          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs
+        },
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "#feb58b",
+          secondary: "#d3dbf4",
+          accent: "#bb3c43",
+          neutral: "#12161c",
+          "neutral-content": "#fffffc",
+          "base-100": "#03080E",
+          "base-content": "#fffffc",
+
+          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
