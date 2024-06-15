4c41f9a46013672438b71df06a2b69fb3b76788b
Tomas <tomas.casabona@gmail.com>
2024-06-15 09:26:19 +0200

    Total completed routes completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "@reduxjs/toolkit/query";
+import { calculateCompletedRoutes } from "../services/calculateCompletedRoutes";
 
 export const Home: React.FC = () => {
   const usuario = useSelector((state: RootState) => state.user);
+  const routesFirebase = useSelector((state: RootState) => state.routes.data);
 
+  const totalCompletedRoutes: number = calculateCompletedRoutes(routesFirebase);
   return (
     <>
       <h2>Hello {usuario ? usuario.info.displayName : "Guest"}</h2>
       <h2>Hello {usuario ? usuario.info.email : "Guest"}</h2>
       <img src={usuario.info.photoURL} alt="" />
-      <p>QUE CHICO MAS GUAPETON</p>
+      <p>QUE CHICO MAS GUAPETON !!</p>
+      <p>Completed Routes : {totalCompletedRoutes}</p>
     </>
   );
 };
