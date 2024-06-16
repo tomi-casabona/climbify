0f80929afee0b8d940cef6c6ad9108bd334d9cef
Tomas <tomas.casabona@gmail.com>
2024-06-16 22:13:10 +0200

    Fixing auhenicated bug in routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,26 +2,26 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { Signin } from "../pages/Signin";
 import { Home } from "../pages/Home";
-import { ProtectedRoute } from "./ProtectedRote";
+import { ProtectedRoute } from "./ProtectedRoute";
 import { Formulario } from "../components/Formulario";
 import { RouteList } from "../pages/RouteList";
 import { PublicRoute } from "./PublicRoute";
 
 export const AppRoutes = ({ isLogged }: { isLogged: boolean }) => {
-	return (
-		<Routes>
-			<Route
-				element={<PublicRoute isAuthorized={isLogged} redirectToPath="/" />}
-			>
-				<Route path="/login" element={<Login />} />
-				<Route path="/signin" element={<Signin />} />
-			</Route>
-			<Route element={<ProtectedRoute isAuthorized={isLogged} />}>
-				<Route path="/" element={<Home />} />
-				<Route path="/routes" element={<RouteList />} />
-				<Route path="/newroute" element={<Formulario />} />
-			</Route>
-			<Route path="*" element={<Navigate to="/" />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route
+        element={<PublicRoute isAuthorized={isLogged} redirectToPath="/" />}
+      >
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<Signin />} />
+      </Route>
+      <Route element={<ProtectedRoute isAuthorized={isLogged} />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/routes" element={<RouteList />} />
+        <Route path="/newroute" element={<Formulario />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,17 +1,10 @@
-import { ReactElement } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 interface PublicRouteProps {
-	isAuthorized: boolean;
-	redirectToPath?: string;
+  isAuthorized: boolean;
+  redirectToPath: string;
 }
 
-export const PublicRoute = ({
-	isAuthorized,
-	redirectToPath = "/",
-}: PublicRouteProps): ReactElement => {
-	if (isAuthorized) {
-		return <Navigate to={redirectToPath} replace />;
-	}
-	return <Outlet />;
+export const PublicRoute = ({ isAuthorized, redirectToPath }: PublicRouteProps) => {
+  return isAuthorized ? <Navigate to={redirectToPath} /> : <Outlet />;
 };
