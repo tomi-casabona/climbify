c42b89997bcac7c6764f70ada1ed577b8d11c5f1
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-06-17 21:55:13 +0200

    logica routeattempt funciona

--- src/pages/RoutePage.tsx
+++ src/pages/RoutePage.tsx
@@ -47,8 +47,9 @@ export const RoutePage = () => {
 		const indexAttempt = route.routeAttempts.findIndex(
 			(attempt) => attempt.id === updatedAttempt.id
 		);
-		const updatedRoute: Route = { ...route };
-		updatedRoute.routeAttempts[indexAttempt] = {...updatedAttempt};
+		const updatedRoute: Route = { ...route, routeAttempts: [...route.routeAttempts] };
+
+		updatedRoute.routeAttempts[indexAttempt] = { ...updatedAttempt };
 
 		console.log(updatedRoute);
 		// Find the route by ID and update it
