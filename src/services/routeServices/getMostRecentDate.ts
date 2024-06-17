798f65306a9c221436d0b9cbfb37d0f773c481b0
Tomas <tomas.casabona@gmail.com>
2024-06-17 22:32:45 +0200

    fix problems in getMostRecentDate

--- src/services/routeServices/getMostRecentDate.ts
+++ src/services/routeServices/getMostRecentDate.ts
@@ -1,8 +1,15 @@
 import type { Attempt } from "../../types/dataTypes";
 
-export const getMostRecentDate = (attempts: Attempt[]): Date => {
+export const getMostRecentDate = (attempts: Attempt[]) => {
+    if (attempts.length === 0) return "";
     const sortedAttempts = attempts.slice().sort((a, b) => {
         return new Date(b.date).getTime() - new Date(a.date).getTime();
     });
-    return new Date(sortedAttempts[0].date);
+    const lastAttempt = new Date(sortedAttempts[0].date)
+    const day = lastAttempt.getDay()
+    const month = lastAttempt.getMonth()
+    const year = lastAttempt.getFullYear()
+
+    return day + "/" + month + "/" + year
+
 };
