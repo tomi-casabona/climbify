0dc3f2478daceab297e56eff36b70da43a536433
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-07-07 01:10:16 +0200

    fixed edit route bug

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.climbify.app',
+  appName: 'Climbify',
+  webDir: 'dist'
+};
+
+export default config;
